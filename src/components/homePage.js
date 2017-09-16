557bbdee58cddf9c013d0a04348fcd29b1949e95
gustavo <gsinacio94@gmail.com>
2017-09-16 16:46:49 -0300

    Fixing CodeClimate Issues

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class HomePage extends Component {
-  render() {
-    return (
-      <div>Home Page!</div>
-    );
-  }
-}
+const HomePage = () => {
+  return (
+    <div>Home Page!</div>
+  );
+};
 
 export default HomePage;

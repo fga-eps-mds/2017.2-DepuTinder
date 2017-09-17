4fe8fa176c18fee32781266773c2e0e170007002
Vítor Xoteslem <vitorcx010@gmail.com>
2017-09-16 22:49:22 -0300

    Visual changes on question container

--- src/containers/question.js
+++ src/containers/question.js
@@ -2,16 +2,25 @@ import React from 'react';
 
 const Question = () => {
   return (
-    <div className="row">
-      <div className="col s12 m6">
-        <div className="card">
-          <div className="card-image">
-            <span className="card-title">Card Title</span>
-            <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">help</i></a>
-          </div>
-          <div className="card-content"><p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-          </div>
-        </div>
+    <div className="card" id="question-card">
+      <div className="card-content yellow acent-3">
+        <center>
+          <h3 className="grey-text text-darken-3"><b>Votação X</b></h3>
+          <span className="card-title grey-text text-darken-3"><b>Titulo da proposição</b><i className="material-icons activator right">help</i></span>
+          <p className="align-left">Sub-Titulo da proposição</p>
+          <ul>
+            <li><a className="waves-effect waves-light btn grey darken-3">Sou a favor</a></li>
+            <br />
+            <li><a className="waves-effect waves-light btn grey darken-3">Sou contra</a></li>
+            <br />
+            <li><a className="waves-effect waves-light btn grey darken-3">Me abstenho</a></li>
+          </ul>
+        </center>
+      </div>
+      <div className="card-reveal">
+        <span className="card-title grey-text text-darken-4">Titulo da Proposição<i className="material-icons right">close</i></span>
+        <p>Descrição da proposição</p>
+        <a className="proposition-link" href="">Saiba mais(link para a descrição)</a>
       </div>
     </div>
   );

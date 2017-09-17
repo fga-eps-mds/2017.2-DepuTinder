b74fa1a573f67e46859cfc06ebfc877946488f25
Amanda Muniz <amanda.muniiz@outlook.com>
2017-09-16 22:24:01 -0300

    Button creation.

---
+++ src/components/answerButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AnswerButton = () => {
+  return (
+    <div>
+      <a className="waves-effect waves-light btn">button</a>
+    </div>
+  );
+};
+
+export default AnswerButton;
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import NavBar from './navBar';
+import AnswerButton from './answerButton';
 
 const HomePage = () => {
   return (
-    <div className="col s12 m12 l12">
-      <NavBar />
+    <div>
+      <div>
+        <NavBar />
+      </div>
+
+      <div className="row s4 m4 l4">
+        <div className="col s4 m4 l4">
+          <AnswerButton />
+        </div>
+      </div>
+
     </div>
   );
 };

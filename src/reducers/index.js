17f130abc5af390ae58facb186ba5e9d5dfcddcd
Calebe Rios <calebe_rios@hotmail.com>
2017-09-19 20:16:10 -0300

    Adding action and reducer to get data for questions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import answeredQuestionReducer from './answeredQuestionReducer';
+import questionDataReducer from './questionDataReducer';
 
 const rootReducer = combineReducers({
   answeredQuestions: answeredQuestionReducer,
+  questionData: questionDataReducer,
 });
 
 export default rootReducer;

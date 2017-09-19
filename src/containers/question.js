40710a8f6279f1d13b944d02cbe4a3c819f00fa7
Calebe Rios <calebe_rios@hotmail.com>
2017-09-19 16:41:53 -0300

    Changing from pure function to class

--- src/containers/question.js
+++ src/containers/question.js
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import FetchQuestionData from '../actions/fetchQuestionData';
 
-const Question = () => {
-  return (
-    <div className="card" id="question-card">
-      <div className="card-content yellow acent-3">
-        <center>
-          <h3 className="grey-text text-darken-3"><b>Votação X</b></h3>
-          <span className="card-title grey-text text-darken-3"><b>Titulo da proposição</b><i className="material-icons activator right">help</i></span>
-          <p className="align-left">Sub-Titulo da proposição</p>
-          <ul>
-            <li><a className="waves-effect waves-light btn grey darken-3">Sou a favor</a></li>
-            <br />
-            <li><a className="waves-effect waves-light btn grey darken-3">Sou contra</a></li>
-            <br />
-            <li><a className="waves-effect waves-light btn grey darken-3">Me abstenho</a></li>
-          </ul>
-        </center>
-      </div>
-      <div className="card-reveal">
-        <span className="card-title grey-text text-darken-4">Titulo da Proposição<i className="material-icons right">close</i></span>
-        <p>Descrição da proposição</p>
-        <a className="proposition-link" href="">Saiba mais(link para a descrição)</a>
-      </div>
-    </div>
-  );
-};
+class Question extends Component {
 
-export default Question;
+  constructor(props) {
+    super(props);
+    this.state = { questions: [] };
+  }
+
+  render() {
+    return (
+      <div className="card" id="question-card">
+        <div className="card-content yellow acent-3">
+          <center>
+            <h3 className="grey-text text-darken-3"><b>Votação X</b></h3>
+            <span className="card-title grey-text text-darken-3"><b>Titulo da proposição</b><i className="material-icons activator right">help</i></span>
+            <p className="align-left">Sub-Titulo da proposição</p>
+            <ul>
+              <li><a className="waves-effect waves-light btn grey darken-3">Sou a favor</a></li>
+              <br />
+              <li><a className="waves-effect waves-light btn grey darken-3">Sou contra</a></li>
+              <br />
+              <li><a className="waves-effect waves-light btn grey darken-3">Me abstenho</a></li>
+            </ul>
+          </center>
+        </div>
+        <div className="card-reveal">
+          <span className="card-title grey-text text-darken-4">Titulo da Proposição<i className="material-icons right">close</i></span>
+          <p>Descrição da proposição</p>
+          <a className="proposition-link" href="">Saiba mais(link para a descrição)</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    answeredQuestions: state.answeredQuestions,
+  };
+}
+
+export default connect(mapStateToProps, { FetchQuestionData })(Question);

ecc8dc2126940f7e29124feaafb20290a33e2f7a
Calebe Rios <calebe_rios@hotmail.com>
2017-09-20 19:29:58 -0300

    Fetching datas from json and render the questions on questionnaire

--- src/containers/question.js
+++ src/containers/question.js
@@ -1,42 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { FetchAnswerQuestion } from '../actions/fetchAnswerQuestion';
+import { FetchQuestionData } from '../actions/fetchQuestionData';
 
 class Question extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { questions: [] };
+    this.state = {
+    };
   }
 
-  render() {
-    const id = this.props.id;
+  renderCard() {
+    const ID = this.props.actualQuestionID;
+    const question = this.props.questionData.questions;
     return (
       <div className="card" id="question-card">
         <div className="card-content yellow acent-3">
           <center>
-            <h3 className="grey-text text-darken-3"><b>Votação { this.props.id }</b></h3>
-            <span className="card-title grey-text text-darken-3"><b>{ this.props.title }</b><i className="material-icons activator right">help</i></span>
-            <p className="align-left">{ this.props.subTitle }</p>
+            <h3 className="grey-text text-darken-3"><b>Votação { question[ID].questionID }</b></h3>
+            <span className="card-title grey-text text-darken-3"><b>{ question[ID].questionTitle }</b><i className="material-icons activator right">help</i></span>
+            <p className="align-left">{ question[ID].questionSubTitle }</p>
             <ul>
-              <li><a className="waves-effect waves-light btn grey darken-3" onClick={() => this.props.sendAnswer({ id, answer: 'SIM' })}>
+              <li><a className="waves-effect waves-light btn grey darken-3" onClick={() => this.props.sendAnswer({ ID, answer: 'SIM' })}>
                 Sou a favor
                 </a>
               </li>
               <br />
-              <li><a className="waves-effect waves-light btn grey darken-3" onClick={() => this.props.sendAnswer({ id, answer: 'NÃO' })}>
+              <li><a className="waves-effect waves-light btn grey darken-3" onClick={() => this.props.sendAnswer({ ID, answer: 'NÃO' })}>
                 Sou contra</a></li>
               <br />
-              <li><a className="waves-effect waves-light btn grey darken-3" onClick={() => this.props.sendAnswer({ id, answer: 'ME ABSTENHO' })}>
+              <li><a className="waves-effect waves-light btn grey darken-3" onClick={() => this.props.sendAnswer({ ID, answer: 'ME ABSTENHO' })}>
                 Me abstenho</a></li>
             </ul>
           </center>
         </div>
         <div className="card-reveal">
-          <span className="card-title grey-text text-darken-4">{ this.props.title }<i className="material-icons right">close</i></span>
-          <p>{ this.props.questionDescription }</p>
+          <span className="card-title grey-text text-darken-4">{ question[ID].questionTitle }<i className="material-icons right">close</i></span>
+          <p>{ question[ID].questionDescription }</p>
           <a className="proposition-link" href="">Saiba mais(link para a descrição)</a>
         </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        { this.props.questionData.questions === undefined ?
+          <h1>assad</h1>
+          : this.renderCard()
+        }
       </div>
     );
   }
@@ -46,11 +60,15 @@ function mapStateToProps(state) {
   return {
     answeredQuestions: state.answeredQuestions,
     questionData: state.questionData,
+    actualQuestionID: state.actualQuestionID,
   };
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    getDataForQuestion() {
+      dispatch(FetchQuestionData());
+    },
     sendAnswer(answer) {
       dispatch(FetchAnswerQuestion(answer));
     },
--- src/containers/questionnaire.js
+++ src/containers/questionnaire.js
@@ -1,42 +1,71 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { FetchQuestionData } from '../actions/fetchQuestionData';
+import { SaveActualQuestionID } from '../actions/saveActualQuestionID';
 import Question from './question';
 
 class Questionnaire extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { questions: [] };
+    this.state = { a: 0, b: ['dasd', '12323'] };
   }
 
   isEmpty() {
-    let a = 0;
-
     if (this.props.questionData.questions === undefined) {
       this.props.getDataForQuestion();
-    } else {
-      while (a < 10) {
-        this.state.questions[a] = this.props.questionData.questions[a];
-        a += 1;
-      }
     }
+  }
+
+  proxNumber() {
+    if (this.props.actualQuestionID < 9) {
+      return this.props.actualQuestionID + 1;
+    }
+    return this.props.actualQuestionID;
+  }
+
+  prevNumber() {
+    if (this.props.actualQuestionID > 0) {
+      return this.props.actualQuestionID - 1;
+    }
+    return this.props.actualQuestionID;
+  }
+
+  pages() {
+    return (
+      <ul className="pagination">
+        <li
+          className="disabled"
+          onClick={() => this.props.sendID(this.prevNumber())}
+        ><i className="material-icons">chevron_left</i></li>
+        <li className="waves-effect btn" onClick={() => this.props.sendID(0)}>1</li>
+        <li className="waves-effect btn" onClick={() => this.props.sendID(1)}>2</li>
+        <li className="waves-effect btn" onClick={() => this.props.sendID(2)}>3</li>
+        <li className="waves-effect btn" onClick={() => this.props.sendID(3)}>4</li>
+        <li className="waves-effect btn" onClick={() => this.props.sendID(4)}>5</li>
+        <li className="waves-effect btn" onClick={() => this.props.sendID(5)}>6</li>
+        <li className="waves-effect btn" onClick={() => this.props.sendID(6)}>7</li>
+        <li className="waves-effect btn" onClick={() => this.props.sendID(7)}>8</li>
+        <li className="waves-effect btn" onClick={() => this.props.sendID(8)}>9</li>
+        <li className="waves-effect btn" onClick={() => this.props.sendID(9)}>10</li>
+        <li
+          className="waves-effect"
+          onClick={() => this.props.sendID(this.proxNumber())}
+        ><i className="material-icons">chevron_right</i></li>
+      </ul>
+    );
   }
 
   render() {
     this.isEmpty();
-    let a = 6;
     return (
       <div>
+        <center>{this.pages()}</center>
         { this.props.questionData.questions === undefined ?
           <div className="progress">
             <div className="indeterminate" />
           </div>
-       : <Question id={this.props.questionData.questions[a].questionID}
-         title={this.props.questionData.questions[a].questionTitle}
-         subTitle={this.props.questionData.questions[a].questionSubTitle}
-         questionDescription={this.props.questionData.questions[a].questionDescription}
-       />
+       : <Question />
        }
       </div>
     );
@@ -47,6 +76,7 @@ function mapStateToProps(state) {
   return {
     answeredQuestions: state.answeredQuestions,
     questionData: state.questionData,
+    actualQuestionID: state.actualQuestionID,
   };
 }
 
@@ -54,6 +84,9 @@ const mapDispatchToProps = (dispatch) => {
   return {
     getDataForQuestion() {
       dispatch(FetchQuestionData());
+    },
+    sendID(ID) {
+      dispatch(SaveActualQuestionID(ID));
     },
   };
 };

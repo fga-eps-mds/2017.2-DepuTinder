7c53634f27d6b281325e492bb51a4b1ff8aa0c65
gustavo <gsinacio94@gmail.com>
2017-09-19 21:39:54 -0300

    Reorganizing homepage layout, creating route for the homePagebutton

--- src/components/mainButton.js
+++ src/components/mainButton.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
 
 const MainButton = (props) => {
   return (
     <div className="mainButton">
       <div>
-        <a className="waves-effect waves-light btn grey darken-3 yellow-text text-accent-3">{props.text}</a>
+        <a onClick={() => browserHistory.push('/answerQuestionnaire')} className="waves-effect waves-light btn grey darken-3 yellow-text text-accent-3">{props.text}</a>
       </div>
     </div>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import App from './components/app';
+import Questionnaire from './containers/questionnaire';
 
 export default (
   <Route>
-    <Route exact path="/" component={App} />
+    <Route exact path="/" component={App}>
+      <Route exact path="/answerQuestionnaire" component={Questionnaire} />
+    </Route>
   </Route>
 );

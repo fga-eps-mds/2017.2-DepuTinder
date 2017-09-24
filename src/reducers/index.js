6a402b5cd3565dff3b4a529b20cb47efc469c2fb
Vítor Xoteslem <vitorcx010@gmail.com>
2017-09-23 23:20:39 -0300

    Adding rankingData to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import answeredQuestionReducer from './answeredQuestionReducer';
 import questionDataReducer from './questionDataReducer';
 import actualQuestionIDReducer from './actualQuestionIDReducer';
+import rankingDataReducer from './rankingDataReducer';
 
 const rootReducer = combineReducers({
   answeredQuestions: answeredQuestionReducer,
   questionData: questionDataReducer,
   actualQuestionID: actualQuestionIDReducer,
+  rankingData: rankingDataReducer,
 });
 
 export default rootReducer;

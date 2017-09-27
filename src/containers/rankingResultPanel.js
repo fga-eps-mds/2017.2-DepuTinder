743aaf8daceb7c2e09b40db9c9789812f3d2d1df
Vítor Xoteslem <vitorcx010@gmail.com>
2017-09-27 01:31:20 -0300

    Adding functions to get data to generate collapsible body elements

--- src/containers/rankingResultPanel.js
+++ src/containers/rankingResultPanel.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FetchRankingData } from '../actions/fetchRankingData';
 
 class RankingResultPanel extends Component {
 
@@ -7,15 +9,41 @@ class RankingResultPanel extends Component {
     this.state = {};
   }
 
+  resultArrayIsEmpty() {
+    if (this.props.rankingData.length === 0) {
+      this.props.getResults();
+    }
+  }
+
   render() {
+    this.resultArrayIsEmpty();
+    console.log(this.rankingData);
+    if (this.props.rankingData.length === 0) {
+      return <div className="collapsible-header"><span>Carregando...</span></div>;
+    }
     return (
       <li>
         <div className="collapsible-header"><i className="material-icons black-text">person</i>Combinações com { this.props.percentage }%</div>
-        <div className="collapsible-body"><span>Candidato 1</span></div>
+        { this.props.rankingData[this.props.groupID].candidates.map((rankingData) => {
+          return <div className="collapsible-body"><span>{rankingData.name}</span></div>;
+        }) }
       </li>
     );
   }
-
 }
 
-export default RankingResultPanel;
+function mapStateToProps(state) {
+  return {
+    rankingData: state.rankingData,
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getResults() {
+      dispatch(FetchRankingData());
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RankingResultPanel);

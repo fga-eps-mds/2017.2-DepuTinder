5b2faa1c556db7ddb4768e3df6d79549886b18c8
Tulio Coelho <tulioopc@gmail.com>
2017-09-27 01:35:31 -0300

    Adding some components on test environment

---
+++ test/components/answeredQuestionsPanelTest.js
@@ -0,0 +1,9 @@
+import { renderComponent, expect } from '../test_helper';
+import AnsweredQuestionsPanel from '../../src/components/answeredQuestionsPanel';
+
+describe('AnsweredQuestionsPanel', () => {
+  let component;
+  beforeEach(() => {
+    component = renderComponent(AnsweredQuestionsPanel);
+  });
+});
---
+++ test/components/navBarTest.js
@@ -0,0 +1,9 @@
+import { renderComponent, expect } from '../test_helper';
+import NavBar from '../../src/components/navBar';
+
+describe('NavBar', () => {
+  let component;
+  beforeEach(() => {
+    component = renderComponent(NavBar);
+  });
+});

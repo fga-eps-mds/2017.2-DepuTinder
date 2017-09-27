f0334e22652dd9c24edd919fb94c75c3e1d8a7b8
Luciana Albuquerque <albuquerqueluciana54@gmail.com>
2017-09-27 20:09:33 -0300

    Fixing problemns

--- src/containers/ranking.js
+++ src/containers/ranking.js
@@ -5,6 +5,17 @@ import RankingResultPanel from './rankingResultPanel';
 import { FetchRankingData } from '../actions/fetchRankingData';
 
 class Ranking extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentWillMount() {
+    this.props.getResults();
+  }
+  
   renderRankingPanel() {
     return (
       <ul className="collapsible collapsible-accordion" data-collapsible="accordion">
@@ -20,16 +31,6 @@ class Ranking extends Component {
         <RankingResultPanel percentage="0" groupID="0" />
       </ul>
     );
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  componentWillMount() {
-    this.props.getResults();
   }
 
   render() {

5e4a98a38726c44d0cfccd12dc5319629b7771d6
vitorcx <vitorcx010@gmail.com>
2017-10-06 00:24:55 -0300

    Removing unnecessary div and adding className to div in HomePage

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -3,25 +3,23 @@ import MainButton from './mainButton';
 
 const HomePage = () => {
   return (
-    <div>
-      <div>
-        <center>
-          <i className="large material-icons">people</i>
-          <ul className="homePageItems">
-            <li>
-              <p id="paragraph_home_page">
-              Lorem ipsum dolor sit amet, malorum tibique hendrerit pri cu.
-              Ne utinam deleniti detraxit mel. Vel affert verear ne, iudico
-              voluptua intellegat. Cetero malorum theophrastus mea ex, tempor
-              voluptaria philosophia vel ne.
-              </p>
-            </li>
-            <br />
-            <br />
-            <li><MainButton text="Responder questionário" /></li>
-          </ul>
-        </center>
-      </div>
+    <div className="HomePage">
+      <center>
+        <i className="large material-icons">people</i>
+        <ul className="homePageItems">
+          <li>
+            <p id="paragraph_home_page">
+            Lorem ipsum dolor sit amet, malorum tibique hendrerit pri cu.
+            Ne utinam deleniti detraxit mel. Vel affert verear ne, iudico
+            voluptua intellegat. Cetero malorum theophrastus mea ex, tempor
+            voluptaria philosophia vel ne.
+            </p>
+          </li>
+          <br />
+          <br />
+          <li><MainButton text="Responder questionário" /></li>
+        </ul>
+      </center>
     </div>
   );
 };

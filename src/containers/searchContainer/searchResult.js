c3669fc994efbca431e1ba9f48043a4da44fdf1c
augustovilarins <augusto.vilarins@gmail.com>
2017-10-19 20:58:53 -0200

    Turning searchResult method into a class component

--- src/containers/searchContainer/searchResult.js
+++ src/containers/searchContainer/searchResult.js
@@ -1,39 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
+class SearchResult extends Component {
 
-function selectDeputy(deputy) {
-  return deputy;
-}
+  static selectDeputy(deputy) {
+    return deputy;
+  }
 
+  renderNames() {
+    return this.props.searchResult.map((deputy) => {
+      const NAME = 0;
+      const ID = 1; // Position of ID
+      return (
+        <li
+          className="collection-item"
+          key={deputy[ID]}
+          onClick={() => SearchResult.selectDeputy(deputy)}
+        >
+          <div>
+            <i className="material-icons">
+              account_circle
+            </i>
+            <br />
+            {deputy[NAME]}
+          </div>
+        </li>
+      );
+    });
+  }
 
-function renderNames(props) {
-  return props.searchResult.map((deputy) => {
-    const NAME = 0;
-    const ID = 1; // Position of ID
+  render() {
     return (
-      <li
-        className="collection-item"
-        key={deputy[ID]}
-        onClick={() => selectDeputy(deputy)}
-      >
-        <div>
-          <i className="material-icons">
-            account_circle
-          </i>
-          <br />
-          {deputy[NAME]}
-        </div>
-      </li>
+      <ul className="colllection">
+        {this.renderNames()}
+      </ul>
     );
-  });
+  }
 }
 
-const SearchResult = (props) => {
+SearchResult.defaultProps = {
+  searchResult: ['Não encontrado'],
+};
+
+SearchResult.propTypes = {
+  searchResult: PropTypes.array,
+};
+
+function mapStateToProps(state) {
+  return {
+    parlamentariansData: state.parlamentariansData,
+  };
+}
+
+/*const SearchResult = (props) => {
   return (
     <ul className="collection">
       {renderNames(props)}
     </ul>
   );
-};
+};*/
 
-export default SearchResult;
+export default connect(mapStateToProps, null)(SearchResult);

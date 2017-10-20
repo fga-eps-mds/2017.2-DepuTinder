1179212e8a9c478d4a2c9a0ba89a8ace310ef4ef
augustovilarins <augusto.vilarins@gmail.com>
2017-10-19 22:03:51 -0200

    Styling ShowParlamentary component

--- src/containers/showParlamentary.js
+++ src/containers/showParlamentary.js
@@ -6,13 +6,26 @@ class ShowParlamentary extends Component {
   render() {
     return (
       <div>
-        {this.props.selectedParlamentary[0]}
-        <br />
-        <img src={this.props.selectedParlamentary[1]}></img>
-        <br />
-        {this.props.selectedParlamentary[2]}
-        <br />
-        {this.props.selectedParlamentary[3]}
+        <h4 className="header">{this.props.selectedParlamentary[0]}</h4>
+        <div className="row">
+          <div className="card horizontal" >
+            <div className="col s6">
+              <div className="card-image" id="deputyPhoto">
+                <img src={this.props.selectedParlamentary[1]} />
+              </div>
+            </div>
+            <div className="col s6">
+              <div className="card-stacked">
+                <div className="card-content">
+                  <p><h7>UF: </h7> {this.props.selectedParlamentary[2]} </p>
+                  <br />
+                  <p><h7> PARTIDO: </h7> {this.props.selectedParlamentary[3]} </p>
+                </div>
+                <div className="card-action" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

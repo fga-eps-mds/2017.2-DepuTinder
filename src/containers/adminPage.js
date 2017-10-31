5f99eb193644dc62f0a15dd593baac22c2b45bae
Amanda Muniz <amanda.muniiz@outlook.com>
2017-10-31 21:19:35 -0200

    Creating container AdminPage
    Signed-off-by: Amanda Muniz <amanda.muniiz@outlook.com>
    Signed-off-by: Luciana Ribeiro <albuquerqueluciana54@gmail.com>

---
+++ src/containers/adminPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class AdminPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        AdminPage
+      </div>
+    );
+  }
+}
+
+export default AdminPage;

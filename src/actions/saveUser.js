7239fc8e69c4d892cb5ed457e53cb83ba9ba26b3
Calebe Rios <calebe_rios@hotmail.com>
2017-11-01 22:43:00 -0200

    Adding action to saveUser on API

---
+++ src/actions/saveUser.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const HOST = 'http://localhost:8000/users/';
+
+export default function saveUser(nameParam, emailParam, passwordParam) {
+  axios.post(`${HOST}`, {
+    name: nameParam,
+    email: emailParam,
+    password: passwordParam,
+  })
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}

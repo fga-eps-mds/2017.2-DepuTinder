1a9ece6b4da1e819eb93a47ce3f74342d422107f
Amanda Muniz <amanda.muniiz@outlook.com>
2017-11-02 15:23:22 -0200

    Creating buttons in questionnaire page

--- src/containers/adminPageContainer/adminQuestionnairePage.js
+++ src/containers/adminPageContainer/adminQuestionnairePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
 
 class AdminQuestionnairePage extends Component {
 
@@ -9,8 +10,28 @@ class AdminQuestionnairePage extends Component {
 
   render() {
     return (
-      <div>
-        questionnairePage
+      <div className="row" id="adminQuestionnaireButtonsRow">
+        <div className="col s6 m6 l6" id="backButtonCollumn">
+          <center>
+            <a
+              className="waves-effect waves-light btn black yellow-text text-accent-3"
+              id="backButton"
+              onClick={() => browserHistory.push('/admin')}
+            >
+              <i className="material-icons left" id="backButtonIcon">arrow_back</i>Voltar
+            </a>
+          </center>
+        </div>
+        <div className="col s6 m6 l6" id="sendButtonCollumn">
+          <center>
+            <a
+              className="waves-effect waves-light btn black yellow-text text-accent-3"
+              id="propositionButton"
+            >
+              <i className="material-icons right" id="sendButtonIcon">send</i>Submeter
+            </a>
+          </center>
+        </div>
       </div>
     );
   }

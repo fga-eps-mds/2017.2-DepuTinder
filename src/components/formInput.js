3187f79a514bcddca6efac1319feb1041591feea
vitorcx <vitorcx010@gmail.com>
2017-11-02 02:15:48 -0200

    Creating formInput component

---
+++ src/components/formInput.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class FormInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ inputText: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="input-field" id={this.props.inputDivID}>
+        <input
+          id={this.props.inputID}
+          value={this.state.inputText}
+          onChange={this.handleChange}
+          type={this.props.type}
+          className="validate"
+        />
+        <label htmlFor={this.props.inputID} data-error="Campo Inválido">{this.props.inputID}</label>
+      </div>
+    );
+  }
+}

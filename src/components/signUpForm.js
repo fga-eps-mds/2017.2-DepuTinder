9637f7bf843a7503d4f7373e5fd5a40339f63f3c
gustavosinacio <gsinacio94@gmail.com>
2017-11-02 10:26:46 -0200

    Adjusting style of layout

--- src/components/signUpForm.js
+++ src/components/signUpForm.js
@@ -21,21 +21,32 @@ class SignUpForm extends Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit} id="userSignUpForm">
+        <div
+          className="container"
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: 50,
+          }}
+        >
+          <FormInput inputDivID="nameInputDiv" inputID="Nome" type="text" />
+          <FormInput inputDivID="emailInputDiv" inputID="Email" type="email" />
+          <FormInput inputDivID="passwordInputDiv" inputID="Senha" type="password" />
+          <FormInput inputDivID="passwordConfirmInputDiv" inputID="Confirmar Senha" type="password" />
 
-        <FormInput inputDivID="nameInputDiv" inputID="Nome" type="text" />
-        <FormInput inputDivID="emailInputDiv" inputID="Email" type="email" />
-        <FormInput inputDivID="passwordInputDiv" inputID="Senha" type="password" />
-        <FormInput inputDivID="passwordConfirmInputDiv" inputID="Confirmar Senha" type="password" />
-
-        <div className="form-group" id="buttonGroup">
-          <button
-            className="btn btn-primary btn-lg"
-            id="signUpButton"
-          >
-            Sign Up
-          </button>
+          <div className="form-group" id="buttonGroup">
+            <center>
+              <button
+                className="btn btn-primary btn-lg"
+                style={{ width: 150, backgroundColor: 'black', marginTop: 30 }}
+                id="signUpButton"
+              >
+                Criar conta
+              </button>
+            </center>
+          </div>
         </div>
-
       </form>
     );
   }

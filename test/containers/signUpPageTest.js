cc96a9498c4632b0a0e17e58a39923dbb423ad7f
ricardocanela <ricardo_canela123@hotmail.com>
2017-11-03 18:37:31 -0200

    Testing signUpForm password, confirm password and signUpButton
    Signed-off-by: ricardocanela <ricardo_canela123@hotmail.com>
    Signed-off-by: byronkamal <byronkamal@hotmail.com>

--- test/containers/signUpPageTest.js
+++ test/containers/signUpPageTest.js
@@ -15,4 +15,16 @@ it('render the input userEmail', () => {
     expect(component.find('#emailInputDiv')).to.exist;
 });
 
+it('render the input password', () => {
+    expect(component.find('#passwordInputDiv')).to.exist;
+});
+
+it('render the password confirm input', () => {
+    expect(component.find('#passwordConfirmInputDiv')).to.exist;
+});
+
+it('render signUp button ', () => {
+    expect(component.find('#signUpButton')).to.exist;
+});
+
 });

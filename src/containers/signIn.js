1f821c6001999f2f5b680ccb2ed733dd07a2fadc
ricardocanela <ricardo_canela123@hotmail.com>
2017-11-06 12:26:04 -0200

    created login page render

--- src/containers/signIn.js
+++ src/containers/signIn.js
@@ -11,15 +11,57 @@ class SignIn extends Component {
 
   render() {
     return (
-      <h1> SignIn </h1>
+      <div className="logInPage">
+        <div
+          id="titleLogIn"
+        >
+          <br />
+          Login
+        </div>
+        <br />
+        <div className="row">
+          <div className="col s6 m6 l6">
+            <div className="card" id="cardLogIn">
+              <div className="card-content white accent-3">
+                <form>
+                  <div className="container">
+                    <div className="row">
+                      <div className="input-field col s12" id="inputEmail">
+                        <input id="userEmail" type="text" data-length="50" />
+                        <label htmlFor="userEmail">E-mail</label>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="input-field col s12" id="inputPassword">
+                        <input id="userPassword" type="password" data-length="50" />
+                        <label htmlFor="userPassword">Senha</label>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row" id="adminQuestionButtonsRow">
+          <div className="input-field col s12" id="backButtonCollumn">
+            <center>
+              <a
+                className="waves-effect waves-light btn black yellow-text text-accent-3"
+                id="loginButton"
+                onClick={() => <h1>a</h1>}
+              >Login
+              </a>
+            </center>
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
 function mapStateToProps(state) {
   return {
-    answeredQuestions: state.answeredQuestions,
-    propositions: state.propositions,
   };
 }
 

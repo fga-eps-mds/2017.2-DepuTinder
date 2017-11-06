c739ae50d3ba9eb3ea68b75e17f2a13669c0c6fa
gustavosinacio <gsinacio94@gmail.com>
2017-11-06 16:51:36 -0200

    Fixing input fields' labels, adding props validation & fixing code climate issues

--- src/containers/signUpForm.js
+++ src/containers/signUpForm.js
@@ -1,16 +1,22 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import saveUser from '../actions/saveUser';
 
 // npm install --save redux-form@4.1.3
+// TODO
+// props validation
 
 class SignUpForm extends Component {
-  handleSubmit(event) {
+  static handleSubmit(event) {
     event.preventDefault();
   }
 
-  test = () => {
-    console.log('test');
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputTest: 'test',
+    };
   }
 
   render() {
@@ -21,28 +27,29 @@ class SignUpForm extends Component {
 
     return (
       <form
-        onSubmit={handleSubmit((values) =>
-        saveUser(values.name, values.email, values.password))}
+        onSubmit={handleSubmit((values) => {
+          saveUser(values.name, values.email, values.password);
+        })}
       >
         <div style={{ margin: 50 }}>
           <div className="input-field form-group" id="nameInputDiv">
-            <label>Nome</label>
             <input type="text" className="form-control" {...name} />
+            <label htmlFor={this.props.values.name}>Nome</label>
           </div>
 
           <div className="input-field form-group" id="emailInputDiv">
-              <label>Email</label>
-            <input type="text" className="form-control" {...email} />
+            <input type="email" className="form-control" {...email} />
+            <label htmlFor={this.props.values.email} data-error="Campo inválido">Email</label>
           </div>
 
           <div className="input-field form-group" id="passwordInputDiv">
-            <label>Senha</label>
             <input type="password" className="form-control" {...password} />
+            <label htmlFor={this.props.values.password}>Senha</label>
           </div>
 
           <div className="input-field form-group" id="passwordConfirmInputDiv">
-            <label>Confirmar Senha</label>
             <input type="password" className="form-control" {...passwordConfirm} />
+            <label htmlFor={this.props.values.passwordConfirm}>Confirmar Senha</label>
           </div>
           <center>
             <button
@@ -57,6 +64,18 @@ class SignUpForm extends Component {
     );
   }
 }
+
+SignUpForm.propTypes = {
+  fields: PropTypes.object,
+  values: PropTypes.object,
+  handleSubmit: PropTypes.func,
+};
+
+SignUpForm.defaultProps = {
+  fields: {},
+  handleSubmit() {},
+  values: {},
+};
 
 export default reduxForm({
   form: 'NewSignUpForm',

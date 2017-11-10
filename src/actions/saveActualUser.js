eb6c0d0ce419d266b24b7a7fcd8f6df3fd045afe
Calebe Rios <calebe_rios@hotmail.com>
2017-11-10 16:20:19 -0200

    Changing the request to PUT

--- src/actions/saveActualUser.js
+++ src/actions/saveActualUser.js
@@ -3,15 +3,13 @@ import axios from 'axios';
 export const SAVE_ACTUAL_USER = 'SAVE_ACTUAL_USER';
 
 export const saveActualUser = (email, password) => {
-  const HOST = 'http://localhost:8000/login/';
+  const HOST = 'http://localhost:8000/users/';
   const data = {
     userEmail: email,
     userPassword: password,
   };
 
   const request = axios.put(HOST, data);
-
-  console.log(request);
 
   return {
     type: SAVE_ACTUAL_USER,

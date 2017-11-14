818a7ddf0fc5e99a54efc2d84bb409e5d95f2446
jullianaa <julliana.coutoalmeida@gmail.com>
2017-11-13 22:37:30 -0200

    Creating reducer to question data

---
+++ src/reducers/questionDataReducer.js
@@ -0,0 +1,10 @@
+import { FETCH_QUESTION_DATA } from '../actions/fetchQuestionData';
+
+export default function (state = [], action) {
+  switch (action.type) {
+    case FETCH_QUESTION_DATA:
+      return action.payload.data;
+    default:
+      return state;
+  }
+}

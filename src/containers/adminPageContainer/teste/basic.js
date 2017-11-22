b69a35a37d7015d1ab8579140273ec612ec8da4c
Amanda Muniz <amanda.muniiz@outlook.com>
2017-11-21 22:35:56 -0200

    Rendering questions list in the drag and drop container

--- src/containers/adminPageContainer/teste/basic.js
+++ src/containers/adminPageContainer/teste/basic.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc';
+import AdminListQuestions from '../adminQuestionnaire/adminListQuestions';
 
-const SortableItem = SortableElement(({ value }) => <li>{value}</li>);
-
-const SortableList = SortableContainer(({ items }) => {
-  return (
-    <ul>
-      {items.map((value, index) => (
-        <SortableItem key={index.item} index={index} value={value} />
-      ))}
-    </ul>
-  );
-});
 
 class SortableComponent extends Component {
+
+  static renderQuestionsList() {
+    return (
+      <div>
+        <AdminListQuestions />
+      </div>
+    );
+  }
+
   state = {
     items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
   };
+
   onSortEnd = ({ oldIndex, newIndex }) => {
     this.setState({
       items: arrayMove(this.state.items, oldIndex, newIndex),
@@ -27,4 +27,17 @@ class SortableComponent extends Component {
   }
 }
 
+const SortableItem = SortableElement(({ value }) => <li>{value}</li>);
+
+const SortableList = SortableContainer(() => {
+  return (
+    <ul>
+      <div>
+        <SortableItem
+          value={SortableComponent.renderQuestionsList()}
+        />
+      </div>
+    </ul>
+  );
+});
 export default SortableComponent;

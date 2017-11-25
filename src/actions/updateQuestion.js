895290259ada6cca5f36013834e37a49c1c5166e
lucianaribeiro <albuquerqueluciana54@gmail.com>
2017-11-25 02:33:17 -0200

    Creating updateQuestion action

---
+++ src/actions/updateQuestion.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const HOST = 'http://localhost:8000/updateQuestion';
+
+export default function updateQuestion(questionnaire, title) {
+  axios.put(`${HOST}`, {
+    questionnaireFK: questionnaire,
+    questionTitle: title,
+  });
+}

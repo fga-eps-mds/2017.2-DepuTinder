288841acae8d3403c1ba54f232408f4ccbbd2058
vitorcx <vitorcx010@gmail.com>
2017-11-25 01:16:40 -0200

    Adjusting updateUser params and HOST route

--- src/actions/updateUser.js
+++ src/actions/updateUser.js
@@ -1,12 +1,31 @@
 import axios from 'axios';
 
-const HOST = 'http://localhost:8000/users/';
+const HOST = 'http://localhost:8000/updateUser/';
 
-export default function updateUser(nameParam, emailParam, passwordParam) {
-  axios.put(`${HOST}`, {
-    userName: nameParam,
-    userEmail: emailParam,
-    userPassword: passwordParam,
-    userImage: '',
-  });
+export default function updateUser(
+  oldEmailParam,
+  oldPasswordParam,
+  nameParam,
+  emailParam,
+  passwordParam) {
+  if (passwordParam !== '') {
+    console.log("Updating password");
+    axios.put(`${HOST}`, {
+      userName: nameParam,
+      userEmail: emailParam,
+      userPassword: passwordParam,
+      userImage: '',
+      oldUserEmail: oldEmailParam,
+      oldUserPassword: oldPasswordParam,
+    });
+  } else {
+    console.log("NOOOO Updating password");
+    axios.put(`${HOST}`, {
+      userName: nameParam,
+      userEmail: emailParam,
+      userImage: '',
+      oldUserEmail: oldEmailParam,
+      oldUserPassword: oldPasswordParam,
+    });
+  }
 }

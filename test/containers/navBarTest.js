0ec49bab86d7a4c8b259a01de0f96e330b67436e
vitorcx <vitorcx010@gmail.com>
2017-12-06 02:14:14 -0200

    Refactoring navBar tests

--- test/containers/navBarTest.js
+++ test/containers/navBarTest.js
@@ -1,14 +1,4 @@
-import React from 'react';
-import { createStore } from 'redux';
-import { expect } from '../test_helper';
-import { Provider } from 'react-redux';
-import {
-  renderIntoDocument,
-  findRenderedDOMComponentWithClass,
-  findAllInRenderedTree,
-  isDOMComponent
-} from 'react-addons-test-utils';
-import reducers from '../../src/reducers';
+import { renderComponent, expect } from '../test_helper';
 import NavBar from '../../src/containers/navbar';
 
 const actualAdminUserTest = {
@@ -28,34 +18,21 @@ const actualUserTest = {
 const adminState = {actualUser: actualAdminUserTest};
 const userState = {actualUser: actualUserTest};
 
-function scryRenderedDOMComponentsWithId(component, id) {
-  return findAllInRenderedTree(component, (inst) => {
-    return isDOMComponent(inst) && inst.getAttribute("id") === id;
-  });
-}
-
-describe('NavBar', () => {
+describe('NavBar admin', () => {
   let component;
   beforeEach(() => {
-    component = renderIntoDocument(
-      <Provider store={createStore(reducers, userState)}>
-        <NavBar />
-      </Provider>
-    );
+    component = renderComponent(NavBar, null, adminState)
   });
 
   it('render navBar', () => {
-    const navBar = scryRenderedDOMComponentsWithId(component, 'navBar');
-    expect(navBar).to.exist;
+    expect(component).to.have.class('navBar');
   });
 
-  it('shows components from navbar on PC', () => {
-    const navBar = scryRenderedDOMComponentsWithId(component, '#renderNavBar');
-    expect(navBar).to.exist;
+  it('Render navbar on mobile', () => {
+    expect(component.find('#renderNavBarMobile')).to.exist;
   });
 
-  it('shows components from navbar on Mobile', () => {
-    const navBar = scryRenderedDOMComponentsWithId(component, '#renderNavBarMobile');
-    expect(navBar).to.exist;
+  it('Render navbar on pc', () => {
+    expect(component.find('#renderNavBar')).to.exist;
   });
 });

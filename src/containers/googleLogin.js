e3c19184e83683855445ff7d6c9b29aef5df4f43
byronkamal <byronkamal@hotmail.com>
2017-12-11 22:19:01 -0200

    Some chenges in googleLogin

--- src/containers/googleLogin.js
+++ src/containers/googleLogin.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import GoogleLogin from 'react-google-login';
 import { saveActualUser } from '../actions/saveActualUser';
 
 class GoogleLoginButton extends Component {
 
   static responseGoogle(response) {
-    saveActualUser(response.profileObj.email, response.profileObj.googleId);
-    browserHistory.push('/');
+    console.log(response);
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '' };
   }
 
   render() {
+    if (this.props.actualUser.status === 200) {
+      browserHistory.push('/');
+    }
+
     return (
       <GoogleLogin
         clientId="849502021062-to8gq1f29rkac9jcql2bbvu71orpmntv.apps.googleusercontent.com"
@@ -22,4 +32,28 @@ class GoogleLoginButton extends Component {
   }
 }
 
-export default GoogleLoginButton;
+GoogleLoginButton.propTypes = {
+  actualUser: PropTypes.obj,
+  saveUser: PropTypes.func.isRequired,
+};
+
+GoogleLoginButton.defaultProps = {
+  actualUser: {},
+  saveUser() {},
+};
+
+function mapStateToProps(state) {
+  return {
+    actualUser: state.actualUser,
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    saveUser(email, password) {
+      dispatch(saveActualUser(email, password));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GoogleLoginButton);
